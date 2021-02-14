f9952bc58761b4b76b730a9f812b74938e1a810e
tripperGo <ai_taurus@163.com>
2021-02-15 00:08:17 +0800

    init

---
+++ src/main/java/com/mright/discuss/DiscussApplication.java
@@ -0,0 +1,13 @@
+package com.mright.discuss;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiscussApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DiscussApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mright/discuss/DiscussApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mright.discuss;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscussApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
